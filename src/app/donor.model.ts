b35510965d82f20de237f1b684864faacc021594
Abhinaw Tiwari <abhinawandyou@gmail.com>
2019-05-16 12:58:25 +0530

    donor model updated

--- src/app/donor.model.ts
+++ src/app/donor.model.ts
@@ -2,10 +2,12 @@ export class Donor {
     public name: string;
     public email: string;
     public contact: string;
+    public count: number;
 
-    constructor(name: string, email: string, contact: string) {
+    constructor(name: string, email: string, contact: string, count: number) {
         this.name = name;
         this.email = email;
         this.contact = contact;
+        this.count = count;
     }
 }
