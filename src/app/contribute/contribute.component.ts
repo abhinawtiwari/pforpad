0bffdde4d0dea5c51e61337b04d26da229a5c4b1
Abhinaw Tiwari <abhinawandyou@gmail.com>
2019-05-22 01:03:46 +0530

    resolved page reload issue on contribute btn click

--- src/app/contribute/contribute.component.ts
+++ src/app/contribute/contribute.component.ts
@@ -23,6 +23,6 @@ export class ContributeComponent implements OnInit {
     this.contributed = true;
     this.contriStatus = "Thanks for your contribution!";
     this.donorService.updateDonors(this.nameInput.nativeElement.value, this.phoneInput.nativeElement.value, this.emailInput.nativeElement.value, this.countInput.nativeElement.value);
-    console.log(this.donorService.getDonors());
+    //console.log(this.donorService.getDonors());
   }
 }
